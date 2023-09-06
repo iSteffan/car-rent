1448e7c394a2c5b36271874cad0a2e6d67fd12bd
Stepan Skichko <st.skichko@gmail.com>
2023-09-06 17:26:09 +0300

    update sidebar

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,13 +1,29 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
+import { Link, Wrapper, Sidebar, List } from './Layout.styled';
 
 export const Layout = () => {
   return (
     <>
       <main>
-        <Suspense>
-          <Outlet />
-        </Suspense>
+        <Wrapper>
+          <Sidebar>
+            <List>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/catalog">Catalog</Link>
+              </li>
+              <li>
+                <Link to="/favorites">Favorites</Link>
+              </li>
+            </List>
+          </Sidebar>
+          <Suspense>
+            <Outlet />
+          </Suspense>
+        </Wrapper>
       </main>
     </>
   );
--- src/components/Layout/Layout.styled.js
+++ src/components/Layout/Layout.styled.js
@@ -1,3 +1,35 @@
 import styled from 'styled-components';
+import { NavLink as StyledLink } from 'react-router-dom';
 
+export const Link = styled(StyledLink)`
+  text-decoration: none;
+  color: #212121;
 
+  &.active {
+    color: orange;
+  }
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+`;
+
+export const Sidebar = styled.aside`
+  padding: 30px 40px;
+
+  width: 200px;
+  height: 100%;
+  border: 1px solid black;
+  background-color: rgb(0 0 0 / 6%);
+  box-shadow: inset rgba(0, 0, 0, 0.2) 0px 0px 20px 0px,
+    rgba(0, 0, 0, 0.14) 0px 1px 1px 0px, rgba(0, 0, 0, 0.12) 0px 1px 3px 0px;
+`;
+
+export const List = styled.ul`
+  padding: 0;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  list-style: none;
+`;
