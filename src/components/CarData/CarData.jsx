df3c861df843cdff37e6823e218e573358f9e062
Stepan Skichko <st.skichko@gmail.com>
2023-09-07 17:08:18 +0300

    cardData styling in progress

--- src/components/CarData/CarData.jsx
+++ src/components/CarData/CarData.jsx
@@ -1,7 +1,21 @@
 import { Box, MainDataBox, DetailsBox } from './CarData.styled';
+import { extractCity, extractCountry } from 'utils/extractor';
 
 export const CarData = ({ data }) => {
-  const { make, model, year, rentalPrice } = data;
+  const {
+    make,
+    model,
+    year,
+    rentalPrice,
+    address,
+    rentalCompany,
+    type,
+    mileage,
+    functionalities,
+  } = data;
+
+  const city = extractCity(address);
+  const country = extractCountry(address);
 
   return (
     <Box>
@@ -12,7 +26,15 @@ export const CarData = ({ data }) => {
         </p>
         <p>{rentalPrice}</p>
       </MainDataBox>
-      <DetailsBox></DetailsBox>
+      <DetailsBox>
+        <p>{city}</p>
+        <p>{country}</p>
+        <p>{rentalCompany}</p>
+        <p>{type}</p>
+        <p>{model}</p>
+        <p>{mileage}</p>
+        <p>{functionalities[0]}</p>
+      </DetailsBox>
     </Box>
   );
 };
--- src/components/CarData/CarData.styled.js
+++ src/components/CarData/CarData.styled.js
@@ -17,7 +17,31 @@ export const MainDataBox = styled.div`
 `;
 
 export const DetailsBox = styled.div`
+  margin-top: 8px;
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  row-gap: 4px;
+
+  height: 40px;
+  overflow: hidden;
+  /* white-space: nowrap;
+  text-overflow: ellipsis; */
+
+  p {
+    margin: 0;
+    font-size: 12px;
+    line-height: 1.5;
+    color: rgba(18, 20, 23, 0.5);
+  }
+
+  p + p::before {
+    content: '';
+    width: 6px;
+    height: 16px;
+    margin: 0 6px;
+    border: 1px solid rgba(18, 20, 23, 0.1);
+  }
   width: 100%;
-  height: auto;
   outline: 1px solid gray;
 `;
