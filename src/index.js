7b9e3674d76f09128b76861aa2ff03aa0232329c
Stepan Skichko <st.skichko@gmail.com>
2023-09-08 13:46:07 +0300

    Revert "Revert "delete redux persist""
    This reverts commit 552598cbec7a505876ed03c34922ea8b26f1d775.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { persistor, store } from 'redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
+import { store } from 'redux/store';
+
 import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
 import './index.css';
@@ -10,11 +10,9 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/car-rent">
-          <App />
-        </BrowserRouter>
-      </PersistGate>
+      <BrowserRouter basename="/car-rent">
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
