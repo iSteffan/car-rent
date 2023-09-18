6cbfb6283bd5090230066ca1b273a37374129c96
Stepan Skichko <st.skichko@gmail.com>
2023-09-18 12:06:43 +0300

    add loader & error message to catalog page

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -5,10 +5,16 @@ import {
   fetchAdvertsPerPage,
   fetchAdvertsAmount,
 } from 'redux/advertOperations';
-import { selectAdverts, selectAmount } from 'redux/advertSlice';
+import {
+  selectAdverts,
+  selectAmount,
+  selectIsLoading,
+  selectError,
+} from 'redux/advertSlice';
 import { CarCard } from 'components/CarCard/CarCard';
 import { List, Container } from './Catalog.styled';
 import { LoadMoreBtn } from 'components/LoadMoreBtn/LoadMoreBtn';
+import { Loader } from 'components/Loader/Loader';
 
 const Catalog = () => {
   const dispatch = useDispatch();
@@ -24,11 +30,20 @@ const Catalog = () => {
     setPage(prevPage => prevPage + 1);
   };
 
+  const error = useSelector(selectError);
+
+  const isLoading = useSelector(selectIsLoading);
   const items = useSelector(selectAdverts);
   const itemsAmount = useSelector(selectAmount);
 
   return (
     <Container>
+      {isLoading && (
+        <div style={{ marginTop: '200px' }}>
+          <Loader />
+        </div>
+      )}
+
       <List>
         {items.map(item => (
           <CarCard key={item.id} item={item} />
@@ -38,6 +53,8 @@ const Catalog = () => {
       {itemsAmount.length - items.length > 0 && (
         <LoadMoreBtn onClick={onLoadMoreClick} />
       )}
+
+      {error && <p>Oops, something wrong is going on...</p>}
     </Container>
   );
 };
