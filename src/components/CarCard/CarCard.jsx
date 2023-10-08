72b36e55e55d901508db2a1f02abbea6004a62bc
Stepan Skichko <st.skichko@gmail.com>
2023-10-08 15:26:56 +0300

    add empty favor text & comment logic with incorrect img

--- src/components/CarCard/CarCard.jsx
+++ src/components/CarCard/CarCard.jsx
@@ -4,6 +4,7 @@ import { CarData } from 'components/CarCard/components/CarData/CarData';
 import { LearnMoreBtn } from 'components/CarCard/components/LearnMoreBtn/LearnMoreBtn';
 import { AddToFavorBtn } from 'components/CarCard/components/AddToFavorBtn/AddToFavorBtn';
 import { ModalWindow } from 'components/ModalWindow/ModalWindow';
+// import Car from '../../img/empty.jpg';
 
 export const CarCard = ({ item }) => {
   const [isTaskModalOpened, setIsTaskModalOpened] = useState(false);
@@ -12,6 +13,11 @@ export const CarCard = ({ item }) => {
   return (
     <li>
       <CardContainer>
+        {/* {item.img.endsWith('.jpeg') || item.img.endsWith('.jpeg') ? (
+          <Image src={item.img} alt={item.description} />
+        ) : (
+          <Image src={Car} alt={item.description} />
+        )} */}
         <Image src={item.img} alt={item.description} />
         <AddToFavorBtn data={item} />
         <CarData data={item} />
--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -2,17 +2,22 @@ import { useSelector } from 'react-redux';
 import { selectFavorites } from 'redux/favor/favoritesSlice';
 import { CarCard } from 'components/CarCard/CarCard';
 import { List, Container } from './Catalog.styled';
+import { EmptyText } from './Favorites.styled';
 
 const Favorites = () => {
   const favorites = useSelector(selectFavorites);
 
   return (
     <Container>
-      <List>
-        {favorites.map(item => (
-          <CarCard key={item.id} item={item} />
-        ))}
-      </List>
+      {favorites.length > 0 ? (
+        <List>
+          {favorites.map(item => (
+            <CarCard key={item.id} item={item} />
+          ))}
+        </List>
+      ) : (
+        <EmptyText>Your favorite list is empty</EmptyText>
+      )}
     </Container>
   );
 };
